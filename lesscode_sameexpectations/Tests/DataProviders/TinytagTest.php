230c0b0d73a8cbd3a6639f9bf8e5b5b2ec6557d3
iSac the Bearded and Terrible <ifoster@appnexus.com>
2013-05-09 06:26:20 +0000

    Added Data Provider Test Files

---
+++ lesscode_sameexpectations/Tests/DataProviders/TinytagTest.php
@@ -0,0 +1,118 @@
+<?php 
+
+class TinytagTest extends PHPUnit_Framework_TestCase {
+	
+	protected static $db;
+	
+	protected $tinyTagModel;
+	
+	public static function setUpBeforeClass() {
+		global $config;
+		$db = new MysqlSimplePDO($config['hostname'], $config['username'], $config['password'], $config['database']);
+		self::$db = $db;
+	}
+	
+	public function setUp() {
+		$this->tinyTagModel = new Model_Tinytag(self::$db);
+	}
+	
+	/**
+	 * @expectedException InvalidArgumentException
+	 */
+	public function testCreateThrowsInvalidArgumentExceptionIfUrlIsBad() {
+		$this->tinyTagModel->create(array('url' => ''));
+	}
+	
+	/**
+	 * @expectedException InvalidArgumentException
+	 */
+	public function testUpdateThrowsInvalidArgumentExceptionIfUrlIsBlank() {
+		$this->tinyTagModel->update(12, array('url' => ''));
+	}
+	
+	/**
+	 * @expectedException InvalidArgumentException
+	 */
+	public function testUpdateThrowsInvalidArgumentExceptionIfUrlIsInvalidString() {
+		$this->tinyTagModel->update(12, array('url' => 'not a url'));
+	}
+	
+	/**
+	 * @expectedException InvalidArgumentException
+	 */
+	public function testUpdateThrowsInvalidArgumentExceptionIfUrlIsMissingProtocol() {
+		$this->tinyTagModel->update(12, array('url' => 'www.appnexus.com'));
+	}
+	
+	/**
+	 * @expectedException InvalidArgumentException
+	 */
+	public function testUpdateThrowsInvalidArgumentExceptionIfIdIsNonNumeric() {
+		$this->tinyTagModel->update('nan', array('url' => 'http://www.appnexus.com'));
+	}
+	
+	/**
+	 * @expectedException InvalidArgumentException
+	 */
+	public function testDeleteThrowsInvalidArgumentExceptionIfIdIsNonNumeric() {
+		$this->tinyTagModel->delete('nan');
+	}
+	
+	/**
+	 * @expectedException InvalidArgumentException
+	 */
+	public function testReadThrowsInvalidArgumentExceptionIfIdIsNonNumeric() {
+		$this->tinyTagModel->read('nan');
+	}
+	
+	public function testInsertSucceedsWithValidParameters() {
+		$expectedUrl = 'http://www.appnexus.com';
+		
+		$newId = $this->tinyTagModel->create(array('url' => $expectedUrl));
+		
+		$this->assertInternalType('numeric', $newId);
+		$tinytag = $this->tinyTagModel->read($newId);
+		$this->assertInternalType('array', $tinytag);
+		$this->assertArrayHasKey('url', $tinytag);
+		$this->assertEquals($expectedUrl, $tinytag['url']);
+	}
+	
+	public function testUpdateSucceedsWithValidParameters() {
+		$expectedUrl = 'http://www.appnexus.com';
+		$newId = $this->tinyTagModel->create(array('url' => 'http://www.google.com'));
+		$this->assertInternalType('numeric', $newId);
+		
+		$this->tinyTagModel->update($newId, array('url' => $expectedUrl));
+		
+		$tinytag = $this->tinyTagModel->read($newId);
+		$this->assertInternalType('array', $tinytag);
+		$this->assertArrayHasKey('url', $tinytag);
+		$this->assertEquals($expectedUrl, $tinytag['url']);
+	}
+	
+	public function testDeleteSucceeds() {
+		$newId = $this->tinyTagModel->create(array('url' => 'http://www.google.com'));
+		$this->assertInternalType('numeric', $newId);
+		
+		$this->assertTrue($this->tinyTagModel->delete($newId));
+		
+		$this->assertFalse($this->tinyTagModel->read($newId));
+	}
+	
+	public function testUpdateThrowsRuntimeExceptionIfQueryFails() {
+		$this->markTestIncomplete("How can we test this?");
+	}
+	
+	public function testCreateThrowsRuntimeExceptionIfQueryFails() {
+		$this->markTestIncomplete("How can we test this?");
+	}
+	
+	public function testDeleteThrowsRuntimeExceptionIfQueryFails() {
+		$this->markTestIncomplete("How can we test this?");
+	}
+	
+	public function testReadThrowsRuntimeExceptionIfQueryFails() {
+		$this->markTestIncomplete("How can we test this?");
+	}
+	
+}
